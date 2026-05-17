4a90c61a6a981d2b2c658d58f4f5edae61b5e789
Salma-Mohamedd <S.Mohamed2393@nu.edu.eg>
2026-05-17 06:32:55 +0300

    organize FINAL

--- scripts/06_submit_baseline.ps1
+++ scripts/06_submit_baseline.ps1
@@ -1,5 +1,9 @@
-docker cp .\06_model_baseline.py spark-master:/tmp/06_model_baseline.py
+﻿docker cp .\scripts\pipeline\06_model_baseline.py spark-master:/tmp/06_model_baseline.py
 
-docker exec spark-master /spark/bin/spark-submit `
---master spark://spark-master:7077 `
+docker exec spark-master bash -lc "
+export PYSPARK_PYTHON=python3 && \
+export PYSPARK_DRIVER_PYTHON=python3 && \
+/spark/bin/spark-submit \
+--master spark://spark-master:7077 \
 /tmp/06_model_baseline.py
+"
